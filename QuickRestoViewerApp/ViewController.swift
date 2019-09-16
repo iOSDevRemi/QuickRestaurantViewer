98b4073a8becf5c5ff4528c57bbdcf01baf68e0b
Daniel <daniel.d4@icloud.com>
2019-09-16 23:33:39 +0200

    Initial Commit

---
+++ QuickRestoViewerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickRestoViewerApp
+//
+//  Created by daniel.d4 on 16/09/2019.
+//  Copyright © 2019 daniel.d4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
